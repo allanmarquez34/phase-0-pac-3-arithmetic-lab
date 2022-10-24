b114f5d1431e3dddfcd580f891343f7a4b56921a
Allan Marquez <allan.marquez.2003@icloud.com>
2022-10-24 00:10:26 -0600

    commit tree 1f5378e2500e17f21914b78af9fa9ebbbe2bc376

---
+++ index.js
@@ -0,0 +1,62 @@
+function add() {
+}
+function subtract() {
+}
+function multiply() {
+}
+function divide() {
+}
+function add() {
+    return "someting";
+}
+function add(a, b) {
+    return "something";
+  }
+  function add(a, b) {
+    return a + b;
+  }
+
+  function subtract() {
+    return "someting";
+}
+function subtract(a, b) {
+    return "something";
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function multiply() {
+    return "someting";
+}
+function multiply(a, b) {
+    return "something";
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  function divide() {
+    return "someting";
+}
+function divide(a, b) {
+    return "something";
+  }
+  function divide(a, b) {
+    return a / b;
+  }
+
+  function increment(n) {
+    return n += 1
+  }
+  
+  function decrement(n) {
+    return n -= 1
+  } 
+  function makeInt(string) {
+    return parseInt(string, 10)
+  }
+  
+  function preserveDecimal(string) {
+    return parseFloat(string)
+  }
+  
